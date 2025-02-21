f8e9506a41fc2f7d23c2ddfc2310b9c3305c8097
Kab-31-16 <Kab-31-16@KAB-31-16-PC>
2025-02-21 13:19:03 +0300

    num1

---
+++ Edition.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac25
+{
+    internal class Edition
+    {
+        /// <summary>
+        /// Поля
+        /// </summary>
+        string name;
+        string surname;
+        int year;
+        /// <summary>
+        /// свойство с методами доступа get и set
+        /// </summary>
+        public string Name {
+            get { return (name != "") ? name : "неизвестно"; } 
+            set => name = value; }
+        public string Surname
+        {
+            get { return (surname != "") ? surname : "неизвестно"; }
+            set => surname = value; }
+        public int Year {
+            get { return (year != 0) ? year : 0; }
+            set
+            {if (value < 1990)
+                {
+                    year = 1990;
+                }
+                else if (value > 2024)
+                {
+                    year = 2024;
+                }
+                else
+                {
+                    year = value;
+                }
+            }
+        }
+        /// <summary>
+        /// Конструктор без параметров
+        /// </summary>
+        public Edition() { }
+        /// <summary>
+        /// Конструктор с параметрами
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="surname"></param>
+        /// <param name="year"></param>
+        public Edition(string name,string surname, int year) 
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Year = year;
+        }
+        /// <summary>
+        /// изменить год издания, увеличив или 
+        /// уменьшив его на указанное число лет
+        /// </summary>
+        /// <param name="year"></param>
+        /// <returns></returns>
+        public int ChangePublicationYearRange(int year)
+        {
+            if (year>1990 && year<2024)
+            {
+                return year;
+            }
+            else 
+            {
+                year = 2000;
+            }
+            return year;
+        }
+        /// <summary>
+        /// определить, попадает ли год издания в заданный интервал
+        /// </summary>
+        /// <param name="year"></param>
+        /// <returns></returns>
+        public string FallIntoYear(int year)
+        {
+            if (year > 1990 && year < 2024)
+            {
+                return "Попадает в интервал";
+            }
+            else
+            {
+                return "Не попадает в интервал";
+            }
+        }
+        /// <summary>
+        /// метод для вывода значений об объекте.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return $"Name = {Name}, Surname = {Surname}, Year = {Year}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,21 @@ namespace prac25
     {
         static void Main(string[] args)
         {
+            //объект с именем edition,конструктор без параметров
+            Edition edition = new Edition();
+            Console.WriteLine(edition.ToString());
+            //объект с именем editon,конструктор c параметрами
+            Edition edition2 = new Edition("Лев", "Николаевич", 1908);
+            Console.WriteLine(edition2.ToString());
+            //конструктор с parse
+            Console.WriteLine("Введите имя");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите Фамилию");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Введите год издания");
+            int year = int.Parse(Console.ReadLine());
+            Edition edition3 = new Edition(name, surname, year);
+            Console.WriteLine($"{edition3.ToString()}\n{edition3.ChangePublicationYearRange(year)}\n{edition3.FallIntoYear(year)}");
         }
     }
 }
